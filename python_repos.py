acbc03655b1899da3c926716ddfc322f9a86f4d9
SkymanSky <gokera@ogr.iu.edu.tr>
2022-06-20 22:36:52 +0300

    Response ile ilgili bazı datalar incelenmek üzere shell üzerinde yazdırıldı.

--- python_repos.py
+++ python_repos.py
@@ -8,6 +8,17 @@ print(f"Status code: {r.status_code}")
 
 #API yanıtını bir değişkende sakla.
 response_dict=r.json()
+print(f"Total repositories: {response_dict['total_count']}")
 
 #Sonuçları işle.
-print(response_dict.keys())
+#print(response_dict.keys())
+
+#Repolar hakkındaki bilgileri incele.
+repo_dicts=response_dict['items']
+print(f"Repositories Returned: {len(repo_dicts)}")
+
+#İlk repoyu incele.
+repo_dict=repo_dicts[0]
+print(f"\nKeys: {len(repo_dict)}")
+for key in sorted(repo_dict.keys()):
+    print(key)
