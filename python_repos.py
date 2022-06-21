e4dfaaed1e517e937b9219e30ca06f3daefb4cf8
SkymanSky <gokera@ogr.iu.edu.tr>
2022-06-21 21:21:53 +0300

    Alınan verilerle çubuk grafiği oluşturuldu.

--- python_repos.py
+++ python_repos.py
@@ -1,4 +1,6 @@
 import requests
+from plotly.graph_objs import Bar
+from plotly import offline
 
 #API çağrısı yap ve yanıtı sakla.
 url='https://api.github.com/search/repositories?q=language:python&sort=stars'
@@ -8,7 +10,9 @@ print(f"Status code: {r.status_code}")
 
 #API yanıtını bir değişkende sakla.
 response_dict=r.json()
+repo_names,stars = [],[]
 print(f"Total repositories: {response_dict['total_count']}")
+
 
 #Sonuçları işle.
 #print(response_dict.keys())
@@ -16,19 +20,22 @@ print(f"Total repositories: {response_dict['total_count']}")
 #Repolar hakkındaki bilgileri incele.
 repo_dicts=response_dict['items']
 print(f"Repositories Returned: {len(repo_dicts)}")
+for repo_dict in repo_dicts:
+    repo_names.append(repo_dict['name'])
+    stars.append(repo_dict['stargazers_count'])
 
-#İlk repoyu incele.
-repo_dict=repo_dicts[0]
-print(f"\nKeys: {len(repo_dict)}")
-for key in sorted(repo_dict.keys()):
-    print(key)
+#Görselleştirme yap.
+data=[{
+    'type':'bar',
+    'x':repo_names,
+    'y':stars,
+}]
 
-print("\nSelected information about each repository:")
-#for repo_dict in repo_dicts:
-print(f"Name: {repo_dict['name']}")
-print(f"Owner: {repo_dict['owner']['login']}")
-print(f"Stars: {repo_dict['stargazers_count']}")
-print(f"Repository: {repo_dict['html_url']}")
-print(f"Created: {repo_dict['created_at']}")
-print(f"Updated: {repo_dict['updated_at']}")
-print(f"Description: {repo_dict['description']}")
+my_layout={
+    'title':'Most-Starred Python Projects on GitHub ',
+    'xaxis':{'title':'Repository'},
+    'yaxis':{'title':'Stars'}
+}
+
+fig={'data':data,'layout':my_layout}
+offline.plot(fig,filename='python_repos.html')
